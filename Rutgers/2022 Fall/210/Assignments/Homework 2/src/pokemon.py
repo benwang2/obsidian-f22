81a0f633e50ad2893c1a6d99df852c831307bb7d
benwang2 <eb.benwang@gmail.com>
2022-10-17 12:57:46 -0400

    vault backup: 2022-10-17 12:57:46

--- Rutgers/2022 Fall/210/Assignments/Homework 2/src/pokemon.py
+++ Rutgers/2022 Fall/210/Assignments/Homework 2/src/pokemon.py
@@ -1,6 +1,26 @@
 from dataclasses import dataclass
 from typing import List
 
+weaknesses = {
+    "fire":     ["grass","ice","bug","steel"],
+    "water":    ["fire","ground","rock"],
+    "electric": ["water","flying"],
+    "grass":    ["water","ground","rock"],
+    "ice":      ["grass","ground","flying"],
+    "fighting": ["normal","ice","rock","dark","steel"],
+    "poison":   ["grass","fairy"],
+    "ground":   ["fire","electric","poison","rock","steel"],
+    "flying":   ["grass","fighting","bug"],
+    "psychic":  ["fighting","poison"],
+    "bug":      ["grass","psychic","dark"],
+    "rock":     ["fire","ice","flying","bug"],
+    "ghost":    ["psychic","ghost"],
+    "dragon":   ["dragon"],
+    "dark":     ["psychic","ghost"],
+    "steel":    ["ice","rock","fairy"],
+    "fairy":    ["fighting","dragon","dark"]
+}
+
 @dataclass()
 class Pokemon:
     id: int
