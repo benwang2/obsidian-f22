17fd71f8967a52046e414fef21e5506802a1e169
benwang2 <eb.benwang@gmail.com>
2022-11-21 14:43:30 -0500

    vault backup: 2022-11-21 14:43:30

---
+++ Rutgers/2022 Fall/352/Assignments/project4/notes.py
@@ -0,0 +1,18 @@
+import time
+# send packets
+
+# wait for ACKs
+ACKs = {}
+RTOs = {}
+for seqNo in []:
+    ACKs[seqNo] = time.time()
+
+while True:
+    value = None
+    if value and value.ack:
+        ACKs[value.ack] = 0
+    for (ackNum, t):
+        if time.time() - t > 0.5:
+            RTOs[ackNum] = True
+            # we know this has timed out
+    if any(RTOs.values())
