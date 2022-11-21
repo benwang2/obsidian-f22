7bbeb4e79c6fa9ecc18b87e04f507aa098da0223
benwang2 <eb.benwang@gmail.com>
2022-11-21 13:56:19 -0500

    vault backup: 2022-11-21 13:56:19

--- Rutgers/2022 Fall/210/Assignments/Homework 3/src/run.py
+++ Rutgers/2022 Fall/210/Assignments/Homework 3/src/run.py
@@ -1,20 +1,23 @@
 import pandas as pd
 
-f = pd.read_csv("./data/EuCitiesTemperatures.csv")
-f[['latitude','longitude']] = f[['latitude','longitude']].fillna(f.groupby('country')[['latitude','longitude']].transform('mean'))
+# Setup
+temps = pd.read_csv("./data/EuCitiesTemperatures.csv")
 
-# combined = pd.concat(f[f['longitude'] >= 15], f[f['longitude'] <= 30])
-lon_gte15 = f[f['longitude'] >= 15]
-lon_lte30 = f[f['longitude'] <= 30]
+# Part 1
+temps[['latitude','longitude']] = temps[['latitude','longitude']].fillna(temps.groupby('country')[['latitude','longitude']].transform('mean'))
 
-lat_gte40 = f[f['latitude'] >= 40]
-lat_lte60 = f[f['latitude'] <= 60]
-
+# Part 2
+lon_gte15 = temps[temps['longitude'] >= 15]
+lon_lte30 = temps[temps['longitude'] <= 30]
+lat_gte40 = temps[temps['latitude'] >= 40]
+lat_lte60 = temps[temps['latitude'] <= 60]
 
 lon_in_range = pd.merge(lon_gte15, lon_lte30, how='inner')
 lat_in_range = pd.merge(lat_gte40, lat_lte60, how='inner')
 subset = pd.merge(lon_in_range, lat_in_range, how='inner')
-print(subset)
 
-# print( type((15 <= f['longitude']) & (f['longitude'] < 30)) )
-# f['longitude'].between(15, 30, inclusive=True)
+city_count = subset.value_counts(['country'])
+maximal_cities = city_count[city_count == city_count.max()]
+
+# Part 3
+print(temps.head(1))
