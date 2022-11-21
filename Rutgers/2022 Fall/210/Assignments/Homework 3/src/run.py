90c6beef9cab31445c74800a6f0734e15fa3e079
benwang2 <eb.benwang@gmail.com>
2022-11-21 13:46:17 -0500

    vault backup: 2022-11-21 13:46:17

--- Rutgers/2022 Fall/210/Assignments/Homework 3/src/run.py
+++ Rutgers/2022 Fall/210/Assignments/Homework 3/src/run.py
@@ -3,4 +3,8 @@ import pandas as pd
 f = pd.read_csv("./data/EuCitiesTemperatures.csv")
 f[['latitude','longitude']] = f[['latitude','longitude']].fillna(f.groupby('country')[['latitude','longitude']].transform('mean'))
 
-print(f[60 >= f.longitude])
+combined = pd.concat(f[f['longitude'] >= 15], f[f['longitude'] <= 30])
+print(combined)
+
+# print( type((15 <= f['longitude']) & (f['longitude'] < 30)) )
+# f['longitude'].between(15, 30, inclusive=True)
