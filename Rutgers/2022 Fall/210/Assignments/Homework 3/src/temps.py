73dc60bdbb9b30063f241d76226e59caea545aae
benwang2 <eb.benwang@gmail.com>
2022-11-22 17:03:15 -0500

    vault backup: 2022-11-22 17:03:15

--- Rutgers/2022 Fall/210/Assignments/Homework 3/src/temps.py
+++ Rutgers/2022 Fall/210/Assignments/Homework 3/src/temps.py
@@ -24,7 +24,11 @@ maximal_cities = city_count[city_count == city_count.max()]
 # Part 3
 temps['temperature'] = temps['temperature'].fillna(temps.groupby(['EU','coastline'])['temperature'].transform('mean'))
 
-regiontype_groups = temps.groupby(['EU','coastline']).groups()
-# regiontype_values = []
-print(regiontype_groups[regiontype_groups.groups[0]])
-# plt.bar(regiontype_groups.groups, regiontype_groups)
+grouped_types = temps.groupby(['EU','coastline'])
+regiontype_groups = [g for g in grouped_types.groups]
+regiontype_values = [len(grouped_types.get_group(g)) for g in grouped_types.groups]
+# for group in groups.groups:
+    # print()
+
+plt.bar(grouped_types.groups, regiontype_values)
+plt.show()
