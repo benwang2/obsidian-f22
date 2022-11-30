de9c07734f4f3d95fbfbb1630d651befc3f9a10b
benwang2 <eb.benwang@gmail.com>
2022-11-30 16:01:57 -0500

    vault backup: 2022-11-30 16:01:57

--- Rutgers/2022 Fall/210/Assignments/Homework 3/src/temps.py
+++ Rutgers/2022 Fall/210/Assignments/Homework 3/src/temps.py
@@ -67,11 +67,7 @@ fig, axs = plt.subplots(2, 2, constrained_layout=True)
 for i, index in enumerate(grouped_types.groups):
     ax = axs.flat[i]
     row = grouped_types.get_group(index)
-    # fig.add_subplot(2,2,i+1)
-    # axs[i].title(
-    #     ("EU" if index[0] == "yes" else "Non-EU") + " " +
-    #     ("Coastline" if index[1] == "yes" else "Other")
-    # ) 
+
     ax.scatter(
         row["latitude"],
         row["longitude"],
